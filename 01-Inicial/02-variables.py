32be299c731f337a51d2bdd32f69bd5a45871323
Andrea Santana <andrea.santana0222@gmail.com>
2021-11-10 09:50:36 -0300

    inicial

---
+++ 01-Inicial/02-variables.py
@@ -0,0 +1,9 @@
+hola_mundo="Hola Mundo"
+
+nombreCliente='Juan' #camelCase
+NombreCliente='Juan' #PascalCase
+nombrecliente='Juan' #flat case
+#nombre-cliente='Juan' kebab case or dash case
+nombre_cliente='Juan' #snake_case common in Python
+
+print(hola_mundo)
---
+++ 01-Inicial/03-tipos-datos.py
@@ -0,0 +1,17 @@
+#string
+nombre='Juan'
+
+#integer
+cantidad=200
+
+#floats
+precio=100.20
+
+#boolean
+registrado=True
+
+#tipado dinamico
+edad = 50
+edad= "cincuenta"
+
+
